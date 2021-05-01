6e53176b072662d74aa579389ef268044bda71a1
techguru <niyotokunlola@gmail.com>
2021-05-01 22:15:33 +0100

    i hope it works, think i just found my bug in allowed host

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-c60+#6xr_s-116p_eq4ml!atugokxob#3b3po%2-o96^p@7j+1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
